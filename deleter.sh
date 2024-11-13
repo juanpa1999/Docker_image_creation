e5db18a7301adfd4e3ba3c8ea39475bafb9fa33a
juan pablo castiblanco <101476957+juanpa1999@users.noreply.github.com>
2024-11-13 16:36:07 -0500

    Update deleter.sh to do not delete jenkis contanier

--- deleter.sh
+++ deleter.sh
@@ -1,12 +1,33 @@
-docker rm -f $(docker ps -aq)
+#!/bin/bash
+
+# Define el nombre del contenedor y la imagen de Jenkins que no quieres eliminar
+CONTAINER_NAME="jenkins-blueocean"
+IMAGE_NAME="jenkis-jenkins-blueocean"
+
+# Eliminar todos los contenedores excepto el de Jenkins
+for container_id in $(docker ps -aq); do
+    container_name=$(docker inspect --format="{{.Name}}" "$container_id" | sed 's/^\/\(.*\)/\1/')
+    if [ "$container_name" != "$CONTAINER_NAME" ]; then
+        docker rm -f "$container_id"
+        echo "Contenedor eliminado: $container_name"
+    fi
+done
+
 sleep 1
-echo "contnedor eliminado"
+echo "Contenedores eliminados, excepto $CONTAINER_NAME"
 docker ps -a
 sleep 1
 
-docker rmi $(docker images -aq)
+# Eliminar todas las imágenes excepto la de Jenkins
+for image_id in $(docker images -q); do
+    image_name=$(docker inspect --format="{{.RepoTags}}" "$image_id" | sed 's/[][]//g' | awk -F: '{print $1}')
+    if [ "$image_name" != "$IMAGE_NAME" ]; then
+        docker rmi -f "$image_id"
+        echo "Imagen eliminada: $image_name"
+    fi
+done
 
 sleep 1
+echo "Imágenes eliminadas, excepto $IMAGE_NAME"
 docker images
-echo "imagenes eliminadas"
 sleep 1
