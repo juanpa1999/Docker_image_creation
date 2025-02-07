1ed50b764b11056481f8c7599d870ca4ad0b7513
juan pablo castiblanco <101476957+juanpa1999@users.noreply.github.com>
2025-02-07 15:45:43 -0500

    Delete exec.sh

--- exec.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker compose up -d
-
-sleep 5
-
-docker compose run --rm alembic init alembic
-docker compose run --rm back alembic upgrade head
-docker compose run --rm back alembic revision --autogenerate
